e676f39ab63612233bdbd272b98229609f2d9fab
JuanOrtiz <rvrn2hdp@gmail.com>
2021-09-27 19:16:04 -0300

    agregando vista localidades

--- src/main/java/com/analistas/AgendaMVC/controller/LocalidadesController.java
+++ src/main/java/com/analistas/AgendaMVC/controller/LocalidadesController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 
 @Controller
+@SessionAttributes("localidades")
 public class LocalidadesController {
     
     ProvinciaRepository ProvinciaRepo = new ProvinciaRepository();
